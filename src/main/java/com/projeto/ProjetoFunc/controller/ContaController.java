4fc6ec8fbfbc5990fab74255ad3bd3e10211adc8
Tales de Campos Hernandes <taleshernandes12@gmail.com>
2024-11-27 09:19:59 -0300

    CORS

--- src/main/java/com/projeto/ProjetoFunc/controller/ContaController.java
+++ src/main/java/com/projeto/ProjetoFunc/controller/ContaController.java
@@ -11,7 +11,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/contas")
-@CrossOrigin(origins = "http://localhost:3000")
 public class ContaController {
     @Autowired
     private ContaService contaService;
--- src/main/java/com/projeto/ProjetoFunc/controller/ExtratoController.java
+++ src/main/java/com/projeto/ProjetoFunc/controller/ExtratoController.java
@@ -7,7 +7,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/extrato")
-@CrossOrigin(origins = "http://localhost:3000")
 public class ExtratoController {
     @Autowired
     private ExtratoService extratoService;
