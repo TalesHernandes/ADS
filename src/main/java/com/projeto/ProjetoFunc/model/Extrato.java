889a9d580898bd4a76a3bfdc2671ccbdbb800498
tales <taleshernandes12@gmail.com>
2024-11-27 00:56:24 -0300

    extrato

--- src/main/java/com/projeto/ProjetoFunc/model/Extrato.java
+++ src/main/java/com/projeto/ProjetoFunc/model/Extrato.java
@@ -2,8 +2,6 @@ package com.projeto.ProjetoFunc.model;
 
 import jakarta.persistence.*;
 
-import java.util.Objects;
-
 /**
  -- TABELA EXTRATO PARA PROJETO
  CREATE TABLE Extrato (
--- src/main/java/com/projeto/ProjetoFunc/repository/ExtratoRepository.java
+++ src/main/java/com/projeto/ProjetoFunc/repository/ExtratoRepository.java
@@ -2,7 +2,6 @@ package com.projeto.ProjetoFunc.repository;
 
 import com.projeto.ProjetoFunc.model.Extrato;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 
 public interface ExtratoRepository extends JpaRepository<Extrato, Long> {
 }
